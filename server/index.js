73856fec79317d166ca8d872894efb2616e759ad
Aravind-Natarajan <aravind485528@gmail.com>
2025-06-25 17:30:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
-const router = express.Router();
-require('dotenv').config(); 
+// const router = express.Router();
+// require('dotenv').config(); 
 
 const app = express();
 
