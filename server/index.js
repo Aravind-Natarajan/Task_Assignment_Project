e7a7e9c0f9916db53634fb3e8a9e7491f82dfd6a
Aravind-Natarajan <aravind485528@gmail.com>
2025-06-25 17:13:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // ✅ CORS setup
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://t4teq-task-assignment.netlify.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
